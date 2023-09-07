2917cdcd4c301675b708c0d8f390415fec394cdd
yTz <114835037+yTz-svg@users.noreply.github.com>
2023-09-06 21:55:09 -0300

    Update ProcessInjector.c

--- ProcessInjector.c
+++ ProcessInjector.c
@@ -1,24 +1,29 @@
-#include <iostream>
+#include <stdio.h>
 #include <windows.h>
 
 BOOL InjectDLL(DWORD dwProcessId, LPCSTR lpszDLLPath) {
-    HANDLE  hProcess, hThread;
-    LPVOID  lpBaseAddr, lpFuncAddr;
-    DWORD   dwMemSize, dwExitCode;
-    BOOL    bSuccess = FALSE;
+    HANDLE hProcess, hThread;
+    LPVOID lpBaseAddr, lpFuncAddr;
+    DWORD dwMemSize, dwExitCode;
+    BOOL bSuccess = FALSE;
     HMODULE hKernel32;
 
-    if ((hProcess = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION |
-        PROCESS_VM_WRITE | PROCESS_VM_READ, FALSE, dwProcessId))) {
+    hProcess = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION |
+        PROCESS_VM_WRITE | PROCESS_VM_READ, FALSE, dwProcessId);
+    
+    if (hProcess != NULL) {
         dwMemSize = lstrlen(lpszDLLPath) + 1;
 
-        if ((lpBaseAddr = VirtualAllocEx(hProcess, NULL, dwMemSize, MEM_COMMIT, PAGE_READWRITE))) {
+        lpBaseAddr = VirtualAllocEx(hProcess, NULL, dwMemSize, MEM_COMMIT, PAGE_READWRITE);
+
+        if (lpBaseAddr != NULL) {
             if (WriteProcessMemory(hProcess, lpBaseAddr, lpszDLLPath, dwMemSize, NULL)) {
                 hKernel32 = GetModuleHandle("kernel32.dll");
                 if (hKernel32 != NULL) {
                     lpFuncAddr = GetProcAddress(hKernel32, "LoadLibraryA");
                     if (lpFuncAddr != NULL) {
-                        if ((hThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)lpFuncAddr, lpBaseAddr, 0, NULL))) {
+                        hThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)lpFuncAddr, lpBaseAddr, 0, NULL);
+                        if (hThread != NULL) {
                             WaitForSingleObject(hThread, INFINITE);
                             if (GetExitCodeThread(hThread, &dwExitCode)) {
                                 bSuccess = (dwExitCode != 0) ? TRUE : FALSE;
@@ -42,9 +47,9 @@ int main() {
     BOOL bResult = InjectDLL(dwProcessId, lpszDLLPath);
 
     if (bResult) {
-        std::cout << "DLL injetada com sucesso!" << std::endl;
+        printf("DLL injetada com sucesso!\n");
     } else {
-        std::cerr << "Falha ao injetar a DLL." << std::endl;
+        fprintf(stderr, "Falha ao injetar a DLL.\n");
     }
 
     return 0;
