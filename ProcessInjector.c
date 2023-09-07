adad4346ed211e0af1757de45b83172108efb80f
yTz <114835037+yTz-svg@users.noreply.github.com>
2023-09-06 22:02:13 -0300

    Update ProcessInjector.c

--- ProcessInjector.c
+++ ProcessInjector.c
@@ -1,44 +1,5 @@
+#include "injector.h"
 #include <stdio.h>
-#include <windows.h>
-
-BOOL InjectDLL(DWORD dwProcessId, LPCSTR lpszDLLPath) {
-    HANDLE hProcess, hThread;
-    LPVOID lpBaseAddr, lpFuncAddr;
-    DWORD dwMemSize, dwExitCode;
-    BOOL bSuccess = FALSE;
-    HMODULE hKernel32;
-
-    hProcess = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION |
-        PROCESS_VM_WRITE | PROCESS_VM_READ, FALSE, dwProcessId);
-    
-    if (hProcess != NULL) {
-        dwMemSize = lstrlen(lpszDLLPath) + 1;
-
-        lpBaseAddr = VirtualAllocEx(hProcess, NULL, dwMemSize, MEM_COMMIT, PAGE_READWRITE);
-
-        if (lpBaseAddr != NULL) {
-            if (WriteProcessMemory(hProcess, lpBaseAddr, lpszDLLPath, dwMemSize, NULL)) {
-                hKernel32 = GetModuleHandle("kernel32.dll");
-                if (hKernel32 != NULL) {
-                    lpFuncAddr = GetProcAddress(hKernel32, "LoadLibraryA");
-                    if (lpFuncAddr != NULL) {
-                        hThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)lpFuncAddr, lpBaseAddr, 0, NULL);
-                        if (hThread != NULL) {
-                            WaitForSingleObject(hThread, INFINITE);
-                            if (GetExitCodeThread(hThread, &dwExitCode)) {
-                                bSuccess = (dwExitCode != 0) ? TRUE : FALSE;
-                            }
-                            CloseHandle(hThread);
-                        }
-                    }
-                }
-            }
-            VirtualFreeEx(hProcess, lpBaseAddr, 0, MEM_RELEASE);
-        }
-        CloseHandle(hProcess);
-    }
-    return bSuccess;
-}
 
 int main() {
     DWORD dwProcessId = 12345; // Substitua pelo ID do processo alvo
